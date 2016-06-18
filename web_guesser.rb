8dd1cd01d5500d2366b4d23dc359bda2fd137bb4
riskybiscuit <patrick.w.wentz@gmail.com>
2016-06-18 09:16:38 -0600

    bare bones functionality

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,43 @@
 require 'sinatra'
 require 'sinatra/reloader'
 secret_number = rand(100)
-
 get '/' do 
-  erb :index, :locals => {:secret_number => secret_number}
+  guess = params[:guess].to_i
+  message = send_message(guess, secret_number)
+  erb :index, :locals => {:secret_number => secret_number, :guess => guess, :message => message}
 end
+
+def send_message(guess, secret_number)
+  if guess == secret_number
+    "Correct! The secret_number is #{secret_number}"
+  else
+    off_check(guess, secret_number)
+  end
+end
+
+def off_check(guess, secret_number)
+  if guess > secret_number
+    too_high(guess, secret_number)
+  else
+    too_low(guess, secret_number)
+  end
+end
+
+def too_high(guess, secret_number)
+  if guess - 5 > secret_number
+    "Way too high!"
+  else
+    "Too high!"
+  end
+end
+
+def too_low(guess, secret_number)
+  if guess + 5 < secret_number
+    "Way too low!"
+  else
+    "Too low!"
+  end
+end
+
+
 
